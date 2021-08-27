d9e9c9984b7ccd2ee239dfad936c4d9f40d7d160
Helena-Bali <elenabaltajs@gmail.com>
2021-08-27 12:08:21 +0300

    change currency in basket

--- client/components/basket/footer.jsx
+++ client/components/basket/footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import CurrencySelector from '../main-page/card/currency-selector'
 import CountOfGoodsSelector from './count-of-goods-selector'
 import TotalPriceSelector from './total-price-selector'
 
@@ -9,7 +10,7 @@ const Footer = () => {
         className="absolute bottom-10 bg-blue-500 text-black items-center text-center h-16 w-42  font-bold shadow-lg  p-2 ml-5">
         Product total amount: <CountOfGoodsSelector />
         <div className="product__total_price h-12 w-42 ml-5">
-        Product total price: <TotalPriceSelector /></div>
+        Product total price: <TotalPriceSelector /> <CurrencySelector/></div>
       </footer>
     </div>
   )
--- client/components/basket/total-price-selector.jsx
+++ client/components/basket/total-price-selector.jsx
@@ -2,6 +2,9 @@ import { useSelector } from 'react-redux'
 
 const TotalPriceSelector = () => {
     const totalPrice = useSelector((store) => store.basketReducer.totalPrice)
-    return totalPrice
+    const rate = useSelector((store) =>
+        store.headerReducer.rates[store.headerReducer.currency])
+    const totalPriceFromCurrency = totalPrice * rate
+    return totalPriceFromCurrency.toFixed(2)
 }
 export default TotalPriceSelector
